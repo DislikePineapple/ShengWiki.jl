67482c336f5b15cdf5a1d7d46db53edce161deab
DislikePineapple <sheng_y@tju.edu.cn>
2024-05-03 21:45:05 +0800

    fix format and add README.md

--- docs/pages.jl
+++ docs/pages.jl
@@ -4,13 +4,15 @@ pages = [
     "序言" => "index.md",
     "数学" => Any[
         "data/Mathematics/Algebra/LinearAlgebra.md",
-        "data/Mathematics/Calculus/SingleVariableCalculs.md"],
-    "工具箱" => Any["data/Toolkits/Markdown.md",
+        "data/Mathematics/Calculus/SingleVariableCalculs.md",
+    ],
+    "工具箱" => Any[
+        "data/Toolkits/Markdown.md",
         "data/Toolkits/MKDocs.md",
         "data/Toolkits/Obsidian.md",
         "data/Toolkits/Git.md",
         "data/Toolkits/Vim.md",
         "data/Toolkits/Latex.md",
-        "data/Toolkits/DoublePinyin.md"
-    ]
+        "data/Toolkits/DoublePinyin.md",
+    ],
 ]
