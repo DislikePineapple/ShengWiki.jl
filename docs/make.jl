bb0e7b26be5f2429ce55669f142862652d35bced
DislikePineapple <sheng_y@tju.edu.cn>
2024-05-04 19:37:23 +0800

    reformatter and add markdown file

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,6 @@ using Documenter, ShengWiki
 
 include("pages.jl")
 
-makedocs(sitename="ShengWiki.jl", authors="Sheng Yang", pages=pages)
+makedocs(sitename = "ShengWiki.jl", authors = "Sheng Yang", pages = pages)
 
-deploydocs(repo="github.com/DislikePineapple/ShengWiki.jl.git"; push_preview=true)
+deploydocs(repo = "github.com/DislikePineapple/ShengWiki.jl.git"; push_preview = true)
